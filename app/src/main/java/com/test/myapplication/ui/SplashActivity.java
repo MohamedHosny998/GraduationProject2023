c8a1532231deeb005647222470dcf83d7ddbeef2
hosnyDev <hosny.dev@gmail.com>
2023-11-04 14:36:50 +0200

    add fullscreen and Handler to splash activity

--- app/src/main/java/com/test/myapplication/ui/SplashActivity.java
+++ app/src/main/java/com/test/myapplication/ui/SplashActivity.java
@@ -2,8 +2,14 @@ package com.test.myapplication.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
+
+import com.test.myapplication.MainActivity;
 import com.test.myapplication.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,6 +17,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title and make activity fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
     }
 }
